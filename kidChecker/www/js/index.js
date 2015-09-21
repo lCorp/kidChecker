6879c833e1cdde4d46b287497245e5f5dad28efc
htduy262 <htduy262@gmail.com>
2015-09-22 06:44:21 +0700

    Commit storage process

--- kidChecker/www/js/index.js
+++ kidChecker/www/js/index.js
@@ -47,3 +47,43 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// DuyHT yeah yeah
+//----------------------------------------------------//
+
+function loadDataToMenu() {
+    var db = openDatabase('kidChecker', '1.0', 'Database for kidChecker', 5 * 1024 * 1024);
+	
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM CHILDREN', [], function (tx, result) {
+          
+            var len = result.rows.length;
+			console.log('Len: ' + len);
+            var childId, childName, childBirthday, childSex;
+            for (index = 0; index < len; index++) {
+                var row = result.rows.item(index);
+                childId = row.id;
+                childName = row.childName;
+                childBirthday = row.birthday;
+				childSex = row.sex;
+				
+				itemContent = "<a style='text-align: left;' class='item' href='detail.html?child=" + childId + "'><span class='glyphicon glyphicon-user'></span>&nbsp;&nbsp;&nbsp; " + childName + " </a>";
+				$('#manageChildrenLink').after(itemContent);
+            };
+        }, null);
+    });
+}
+
+function initializeMasterData() {
+    var db = openDatabase('kidChecker', '1.0', 'Database for kidChecker', 5 * 1024 * 1024);
+
+    // CHILDREN
+    db.transaction(function (tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS CHILDREN (id unique, childName, birthday, sex)');
+        tx.executeSql('DELETE FROM CHILDREN');
+        tx.executeSql('INSERT INTO CHILDREN (id, childName, birthday, sex) VALUES (?, ?, ?, ?)', [1, "Huynh Thanh Duy", "1991/06/14", "Boy"]);
+        tx.executeSql('INSERT INTO CHILDREN (id, childName, birthday, sex) VALUES (?, ?, ?, ?)', [2, "Ngo Huu Loc", "1980/04/16", "Girl"]);
+        tx.executeSql('INSERT INTO CHILDREN (id, childName, birthday, sex) VALUES (?, ?, ?, ?)', [3, "Bon Bon", "1990/09/03", "Girl"]);
+    });
+}
+
