c88c40bc1ec5687a1d79bb753fcddf0559a0f030
htduy262 <htduy262@gmail.com>
2015-10-05 17:05:01 +0700

    commit add create.html but has not been done

--- kidChecker/www/js/index.js
+++ kidChecker/www/js/index.js
@@ -64,11 +64,92 @@ function loadDataToMenu() {
                 var row = result.rows.item(index);
                 childId = row.id;
                 childName = row.childName;
-                childBirthday = row.birthday;
-				childSex = row.sex;
 				
-				itemContent = "<a style='text-align: left;' class='item' href='detail.html?child=" + childId + "'><span class='glyphicon glyphicon-user'></span>&nbsp;&nbsp;&nbsp; " + childName + " </a>";
+				itemContent = "<a style='text-align: left;' class='item' href='detail.html?id=" + childId + "'><span class='glyphicon glyphicon-user'></span>&nbsp;&nbsp;&nbsp; " + childName + " </a>";
 				$('#manageChildrenLink').after(itemContent);
+            };
+        }, null);
+    });
+}
+
+function loadDataToIndex() {
+    var db = openDatabase('kidChecker', '1.0', 'Database for kidChecker', 5 * 1024 * 1024);
+	
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM CHILDREN', [], function (tx, result) {
+          
+            var len = result.rows.length;
+			console.log('Len: ' + len);
+            var childId, childName, childBirthday, childSex;
+            for (index = len - 1; index >= 0; index--) {
+                var row = result.rows.item(index);
+                childId = row.id;
+                childName = row.childName;
+				
+				itemContent = "<button onclick='location.href=\"detail.html?id="+ childId +"\"' style='margin-bottom: 10px; text-align: left;' type='button' class='btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12'>" + childName + "<span class='glyphicon glyphicon-chevron-right pull-right'></span></button>";
+
+				$('#listChildren').append(itemContent);
+            };
+        }, null);
+    });
+}
+
+function loadDataToDetail(idChild) {
+    var db = openDatabase('kidChecker', '1.0', 'Database for kidChecker', 5 * 1024 * 1024);
+	
+    db.transaction(function (tx) {
+        tx.executeSql("SELECT * FROM CHILDREN WHERE id="+idChild, [], function (tx, result) {
+          
+            var len = result.rows.length;
+			console.log('idChild: ' + idChild);
+			console.log('Len: ' + len);
+            var childId, childName, childBirthday, childSex;
+            for (index = len - 1; index >= 0; index--) {
+                var row = result.rows.item(index);
+                childId = row.id;
+                childName = row.childName;
+				birthday = row.birthday;
+				sex = row.sex;
+
+				var btnChart = "<button type='button' class='btn pull-right' onclick='location.href=\"chart.html?id="+ childId +"\"'><span class='glyphicon glyphicon-equalizer pull-right'></span></button>";
+
+				$('title').html(childName + "'s information");
+
+				$('#titleName').html("");
+				$('#titleName').append(childName);
+				$('#titleName').append(btnChart);
+				$('#childName').html(childName);
+				$('#birthday #timePicker').val(birthday);
+				$('#sex').val(sex);
+            };
+        }, null);
+    });
+}
+
+function loadDataToChart(idChild) {
+    var db = openDatabase('kidChecker', '1.0', 'Database for kidChecker', 5 * 1024 * 1024);
+	
+    db.transaction(function (tx) {
+        tx.executeSql("SELECT * FROM CHILDREN WHERE id="+idChild, [], function (tx, result) {
+          
+            var len = result.rows.length;
+			console.log('idChild: ' + idChild);
+			console.log('Len: ' + len);
+            var childId, childName, childBirthday, childSex;
+            for (index = len - 1; index >= 0; index--) {
+                var row = result.rows.item(index);
+                childId = row.id;
+                childName = row.childName;
+				birthday = row.birthday;
+				sex = row.sex;
+
+				var btnEdit = "<button type='button' class='btn pull-right' onclick='location.href=\"detail.html?id="+ childId +"\"'><span class='glyphicon glyphicon-edit pull-right'></span></button>";
+
+				$('title').html(childName + "'s chart");
+
+				$('#titleName').html("");
+				$('#titleName').append(childName);
+				$('#titleName').append(btnEdit);
             };
         }, null);
     });
